26971798177ebf7ba10af6c75611b07c32e14113
olusecc <oadewusi@andrew.cmu.edu>
2025-08-10 15:50:44 +0200

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,49 @@
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "network_name" {
+  description = "VPC network name"
+  type        = string
+  default     = "vpc-ansible"
+}
+
+variable "ssh_public_key" {
+  description = "Your full OpenSSH public key line"
+  type        = string
+}
+
+variable "ssh_source_ranges" {
+  description = "CIDRs allowed to SSH"
+  type        = list(string)
+  # Set to your /32 in terraform.tfvars; keep 0.0.0.0/0 only for testing
+  default = ["0.0.0.0/0"]
+}
+
+variable "user_formgt" {
+  type    = string
+  default = "formgt"
+}
+
+variable "user_fortools" {
+  type    = string
+  default = "fortools"
+}
+
+variable "user_formie" {
+  type    = string
+  default = "formie"
+}
